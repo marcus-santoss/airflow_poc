0cb6dfac82875292eeaadd661f4022af1e4163af
marcus.santos <mpsoulbass@gmail.com>
2021-08-25 11:59:12 -0300

    feat: Criação dos scripts de inicialização

--- first_init_docker.sh
+++ first_init_docker.sh
@@ -1,7 +1,16 @@
-podmman-compose up --no-start -V
+# airflow-scheduler - The scheduler monitors all tasks and DAGs, then triggers the task instances once their dependencies are complete.
+# airflow-webserver - The webserver available at http://localhost:8080.
+# airflow-worker - The worker that executes the tasks given by the scheduler.
+# airflow-init - The initialization service.
+# flower - The flower app for monitoring the environment. It is available at http://localhost:5555.
+# postgres - The database.
+# redis - The redis - broker that forwards messages from scheduler to worker.
 
-podman-compose start postgres
-podman-compose start airflow-init && podman-compose logs -f airflow-init
-podman-compose down
-podman-compose up -d
-timeout 15 podman-compose logs -f airflow-webserver
+# Sobe o servico airflow-init ( população do banco de dados )
+docker-compose up airflow-init
+
+# Sobe o ambiente completo
+docker-compose up
+
+# Visualizar os logs do compose
+docker-compose logs
--- first_init_podman.sh
+++ first_init_podman.sh
@@ -1,7 +1,25 @@
-podmman-compose up --no-start -V
+# airflow-scheduler - The scheduler monitors all tasks and DAGs, then triggers the task instances once their dependencies are complete.
+# airflow-webserver - The webserver available at http://localhost:8080.
+# airflow-worker - The worker that executes the tasks given by the scheduler.
+# airflow-init - The initialization service.
+# flower - The flower app for monitoring the environment. It is available at http://localhost:5555.
+# postgres - The database.
+# redis - The redis - broker that forwards messages from scheduler to worker.
 
+# Inicializar cria os containers sem inicializar
+podmman-compose up --no-start --force-recreate -t 0
+
+#Inicializa o Postgres
 podman-compose start postgres
+
+# Inicializa o airflow-init ( popular o banco de dados )
 podman-compose start airflow-init && podman-compose logs -f airflow-init
-podman-compose down
-podman-compose up -d
-timeout 15 podman-compose logs -f airflow-webserver
+
+# Derruba o ambiente
+podman-compose down -t 0
+
+# Sobe o ambiente com todos os componentes
+podman-compose up -d -t 0
+
+# Exibe os logs do webserver
+timeout 30 podman-compose logs -f airflow-webserver
